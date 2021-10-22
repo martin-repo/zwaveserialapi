51bb7ea8f7b7c643755c641c3043f9c7046658ae
Martin <48413705+martin-repo@users.noreply.github.com>
2021-10-22 16:31:09 +0200

    Added documentation

--- src/ZWaveSerialApi.Devices/DeviceType.cs
+++ src/ZWaveSerialApi.Devices/DeviceType.cs
@@ -8,7 +8,10 @@ namespace ZWaveSerialApi.Devices
 {
     public enum DeviceType
     {
+        // [ManufacturerSpecific(ManufacturerId, ProductType, ProductId)]
+        // [ManufacturerSpecific(0x0371, 0x0003, 0x0002)]
         AeotecLedBulb6MultiColor,
+        // [ManufacturerSpecific(0x0086, 0x0002, 0x0064)]
         AeotecMultiSensor6
     }
 }
